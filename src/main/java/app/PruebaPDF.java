9f6165e329c63dc6dde9f23d8373d380354a0bcf
ANDREA CORDERO ESPINOZA <andrea.cordero.espinoza@est.una.ac.cr>
2026-09-03 17:24:57 -0600

    generador de graficos

---
+++ src/main/java/app/PruebaPDF.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.util.List;
+
+import static app.Logic.GeneradorReportePDF.generar;
+
+public class PruebaPDF {
+    public static void main(String[] args) {
+        List<String> encabezados = List.of("Id", "Descripcion");
+        List<List<String>> filas = List.of(
+                List.of("CAT-000001", "Sala para 10 personas"),
+                List.of("CAT-000002", "Laptop windows")
+        );
+        generar("Listado de Categorias", encabezados, filas, "prueba.pdf");
+    }
+}
