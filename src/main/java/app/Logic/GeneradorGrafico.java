4e2a0e47083b706ef526892399a056ffda2a93b0
ANDREA CORDERO ESPINOZA <andrea.cordero.espinoza@est.una.ac.cr>
2026-09-03 17:24:57 -0600

    generador de graficos

---
+++ src/main/java/app/Logic/GeneradorGrafico.java
@@ -0,0 +1,26 @@
+package app.Logic;
+import app.DTO.ResultadoEstadisticaDTO;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+public class GeneradorGrafico {
+    public static Image generar(String titulo, String ejex, String ejey, List<ResultadoEstadisticaDTO> datos){
+        if (datos == null || datos.isEmpty()) {
+            throw new IllegalArgumentException("No hay datos para graficar");
+        }
+
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (ResultadoEstadisticaDTO r : datos) {
+            dataset.addValue(r.getCantidad(), "Cantidad", r.getEtiqueta());
+        }
+
+        JFreeChart chart = ChartFactory.createBarChart(titulo, ejex, ejey, dataset);
+        BufferedImage bufferedImage = chart.createBufferedImage(600, 400);
+        return SwingFXUtils.toFXImage(bufferedImage, null);
+    }
+}
