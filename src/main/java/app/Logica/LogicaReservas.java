b9687d5f297da14acae6a51b53604dd027afdc5e
JP <josepablosanchezqwq@gmail.com>
2026-09-04 13:41:37 -0600

    Logica sin avanzar

---
+++ src/main/java/app/Logica/LogicaReservas.java
@@ -0,0 +1,12 @@
+package app.Logica;
+
+import app.DTO.ReservaDTO;
+import app.Datos.RecursoDatos;
+import app.Datos.ReservaDatos;
+
+public class LogicaReservas {
+    private RecursoDatos recursoDatos;
+    private ReservaDatos reservaDatos;
+
+
+}
