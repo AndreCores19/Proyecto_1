f21baa66520d9c61c5453a60dc9dd1ee559e966b
Emily Benavides <emily.benavides.rodriguez@est.una.ac.cr>
2026-09-07 20:23:49 -0600

    vista funcionario admin

--- src/main/java/app/ui/FuncionarioViewController.java
+++ src/main/java/app/ui/FuncionarioViewController.java
@@ -1,13 +1,14 @@
 package app.ui;
 
 import app.DTO.FuncionarioDTO;
+import app.Logica.GeneradorReportePDF;
 import app.Servicios.ServicioFuncionario;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import app.Logica.GeneradorReportePDF;
+import app.Logica.GeneradorReportePDFLogica;
 
 import java.util.List;
 
@@ -140,9 +141,14 @@ public class FuncionarioViewController {
             filas.add(List.of(f.getId(), f.getNombre(), f.getTelefono()));
         }
 
+        String rutaPdf = "Data/reporteFuncionarios.pdf";
+
         try {
-            GeneradorReportePDF.generar("Listado de Funcionarios", encabezados, filas, "src/Data/reporteFuncionarios.pdf");
-            mostrarExito("Reporte generado correctamente.");
+            GeneradorReportePDFLogica.generar("Listado de Funcionarios", encabezados, filas, "Data/reporteFuncionarios.pdf");
+            java.io.File archivoPdf = new java.io.File(rutaPdf);
+            if (java.awt.Desktop.isDesktopSupported()) {
+                java.awt.Desktop.getDesktop().open(archivoPdf);
+            }
         } catch (Exception e) {
             mostrarError("Error al generar el reporte: " + e.getMessage());
         }
